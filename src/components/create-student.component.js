7117ab18cd7ed9d93d4e9004ee0a4e9226505847
Meyer <meyer.albeiro04@gmail.com>
2022-11-01 16:44:17 -0500

    form create student

--- src/components/create-student.component.js
+++ src/components/create-student.component.js
@@ -1,7 +1,102 @@
 import React, { Component } from "react";
+import axios from "axios";
+
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 export default class CreateStudent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onChangeStudentName = this.onChangeStudentName.bind(this);
+    this.onChangeStudentLastName = this.onChangeStudentLastName.bind(this);
+    this.onChangeStudentEmail = this.onChangeStudentEmail.bind(this);
+    this.onChangeStudentRollno = this.onChangeStudentRollno.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      name: "",
+      lastName: "",
+      email: "",
+      rollno: "",
+    };
+  }
+
+  onChangeStudentName(e) {
+    this.setState({ name: e.target.value });
+  }
+  onChangeStudentLastName(e) {
+    this.setState({ lastName: e.target.value });
+  }
+  onChangeStudentEmail(e) {
+    this.setState({ email: e.target.value });
+  }
+  onChangeStudentRollno(e) {
+    this.setState({ rollno: e.target.value });
+  }
+  onSubmit(e) {
+    e.preventDefault();
+
+    const StudentObject = {
+      name: this.state.name,
+      lastName: this.state.lastName,
+      email: this.state.email,
+      rollno: this.state.rollno,
+    };
+
+    axios
+      .post("http://localhost:4000/students/create-student", StudentObject)
+      .then((res) => console.log(res.data));
+    this.sentence({ name: "", lastName: "", email: "", rollno: "" });
+  }
+
   render() {
-    return <div>Crear estudiante</div>;
+    return (
+      <div class="form-wrapper">
+        <Form onSubmit={this.onSubmit}>
+          <Form.Group controlId="Name">
+            <Form.Label>Nombre</Form.Label>
+            <Form.Control
+              type="text"
+              value={this.state.name}
+              onChange={this.onChangeStudentName}
+            />
+          </Form.Group>
+          <Form.Group controlId="lastName">
+            <Form.Label>Apellido</Form.Label>
+            <Form.Control
+              type="text"
+              value={this.state.lastName}
+              onChange={this.onChangeStudentLastName}
+            />
+          </Form.Group>
+          <Form.Group controlId="Email">
+            <Form.Label>Correo Electrónico</Form.Label>
+            <Form.Control
+              type="email"
+              value={this.state.email}
+              onChange={this.onChangeStudentEmail}
+            />
+          </Form.Group>
+          <Form.Group controlId="Name">
+            <Form.Label>Código</Form.Label>
+            <Form.Control
+              type="text"
+              value={this.state.rollno}
+              onChange={this.onChangeStudentRollno}
+            />
+          </Form.Group>
+          <Button
+            variant="danger"
+            size="lg"
+            block="block"
+            type="submit"
+            className="mt-4"
+          >
+            Crear Estudiantes
+          </Button>
+        </Form>
+      </div>
+    );
   }
 }
